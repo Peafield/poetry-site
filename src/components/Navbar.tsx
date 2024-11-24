c9321b0bd2aa108cf1b567f312e5917965d6e499
peafield <peterjohncoles@gmail.com>
2024-11-24 09:25:27 +0000

    Add BurgerIcon and CloseIcon components; refactor Navbar to use new icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { BurgerIcon, CloseIcon } from "./icons";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -58,35 +59,9 @@ const Navbar = () => {
             className="text-primary focus:outline-none hover:text-secondary"
           >
             {isOpen ? (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <CloseIcon className="w-6 h-6" />
             ) : (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
+              <BurgerIcon className="w-6 h-6" />
             )}
           </button>
         </div>
