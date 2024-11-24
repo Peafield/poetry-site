2b242d5b6721998f2797c41022c5fc3b5165cda3
peafield <peterjohncoles@gmail.com>
2024-11-24 10:58:05 +0000

    Update data type in Home component to use CatergorisedPosts schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
-import { Post } from "./api/posts/postSchema";
+import { CatergorisedPosts } from "./api/posts/postSchema";
 
 export default async function Home() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/posts`);
   if (response.ok) {
-    const data: Post[] = await response.json();
+    const data: CatergorisedPosts[] = await response.json();
     console.log(data);
   } else {
     //TODO: Handle error with a toast or a modal
