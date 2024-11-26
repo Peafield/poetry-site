4cc7665d0e83b75a26f2458f656b2e1993f63d0b
peafield <peterjohncoles@gmail.com>
2024-11-26 14:42:06 +0000

    Add Admin page component and enhance data fetching in Archive and Home pages

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,14 +1,27 @@
 import Archive from "@/components/Archive";
 import { Post } from "../api/posts/postSchema";
 
-export default async function Page() {
+export const dynamic = "force-dynamic";
+
+export default async function ArchivePage() {
   let postData: Post[] | null = null;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/posts`);
-  if (response.ok) {
-    postData = await response.json();
-  } else {
-    //TODO: Handle error with a toast or a modal
-    console.error("Failed to fetch data");
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_APP_URL}/api/posts`,
+      {
+        cache: "no-store",
+      }
+    );
+
+    if (response.ok) {
+      postData = await response.json();
+    } else {
+      console.error("Failed to fetch data: ", response.statusText);
+    }
+  } catch (error) {
+    console.error("Error fetching posts: ", error);
   }
+
   return <Archive posts={postData} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 import Home from "@/components/Home";
 import { Post } from "./api/posts/postSchema";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   let postData: Post[] | null = null;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/posts`);
-  if (response.ok) {
-    postData = await response.json();
-  } else {
-    //TODO: Handle error with a toast or a modal
-    console.error("Failed to fetch data");
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_APP_URL}/api/posts`,
+      {
+        cache: "no-store",
+      }
+    );
+
+    if (response.ok) {
+      postData = await response.json();
+    } else {
+      console.error("Failed to fetch data: ", response.statusText);
+    }
+  } catch (error) {
+    console.error("Error fetching posts: ", error);
   }
 
   return <Home postsData={postData} />;
