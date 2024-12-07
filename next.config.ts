66fd9faf480c6f762f5836dd0d922e867bc2a12a
peafield <peterjohncoles@gmail.com>
2024-12-07 22:43:36 +0000

    Add allowed image domains for remote image loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
+    domains: ["www.wendisworminghallwhimsies.uk", "localhost"],
     remotePatterns: [
       {
         protocol: "http",
